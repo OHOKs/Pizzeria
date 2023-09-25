ab7ff1176ae3396244b3529dac48d3d16e819cbe
OHOKs <85952515+OHOKs@users.noreply.github.com>
2023-09-25 13:05:01 +0200

    Create Etel.js

---
+++ dist/Etel.js
@@ -0,0 +1,40 @@
+class Etel {
+    constructor(nev, kaloriaSzam) {
+        this.nev = nev;
+        if (kaloriaSzam >= 0){
+            this._kaloriaSzam = kaloriaSzam;
+        } else {
+            this._kaloriaSzam = 0;
+        }
+        this._fogyaszthato = true;
+    }
+
+    get kaloriaSzam() {
+        return this._kaloriaSzam;
+    }
+
+    set kaloriaSzam(ertek) {
+        if (ertek >= 0) {
+            this._kaloriaSzam = ertek;
+        } else {
+            this._kaloriaSzam = 0;
+        }
+    }
+
+    get fogyaszthato() {
+        return this._fogyaszthato;
+    }
+
+    set fogyaszthato(ertek) {
+        if (typeof ertek === 'boolean') {
+            this._fogyaszthato = ertek;
+        } else {
+            this._fogyaszthato = false;
+        }
+    }
+
+    info() {
+        const fogyaszthato_szoveg = this._fogyaszthato ? "igen" : "nem";
+        return `${this.nev} (${this._kaloriaSzam} kaloria), fogyaszthato: ${fogyaszthato_szoveg}!`;
+    }
+}
